7a6257146cc77a61fbda74a7b27447c36bf59cd4
Yannis Borel <yannis.borel84@gmail.com>
2026-04-02 15:30:16 +0200

    perf: chargement async + cache données statiques (salles/groupes)

--- src/main/java/projet/M1/MainApp.java
+++ src/main/java/projet/M1/MainApp.java
@@ -2,6 +2,8 @@ package projet.M1;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import projet.M1.BDD.DataCache;
+import projet.M1.BDD.JPAUtil;
 import projet.M1.ui.SceneManager;
 
 /**
@@ -14,12 +16,26 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            // Préchargement du pool JPA + données statiques (salles, groupes)
+            // en arrière-plan pendant que la fenêtre s'affiche
+            Thread preload = new Thread(() -> {
+                JPAUtil.getEntityManagerFactory();
+                DataCache.getInstance().preload();
+            });
+            preload.setDaemon(true);
+            preload.start();
+
             SceneManager.getInstance().init(primaryStage);
             SceneManager.getInstance().showLogin();
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    @Override
+    public void stop() {
+        JPAUtil.close();
     }
 
     public static void main(String[] args) {
